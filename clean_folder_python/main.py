5f5f7efc95d999293e2a703dae77f50cd747dd17
Abderraouf9 <55321747+Abderraouf99@users.noreply.github.com>
2023-01-08 14:57:58 -0500

    rename function parameter of clean_folder

--- clean_folder_python/main.py
+++ clean_folder_python/main.py
@@ -7,10 +7,10 @@ import sys
 logger = ScriptLogger()
 
 
-def clean_folder(file_formats: dict, curr_directory: str):
-    logger.log(f"Starting to clean '{curr_directory}' folder")
+def clean_folder(file_formats: dict, target_directory: str):
+    logger.log(f"Starting to clean '{target_directory}' folder")
 
-    dir_content = os.listdir(curr_directory)
+    dir_content = os.listdir(target_directory)
 
     for d_file in dir_content:
         try:
@@ -21,12 +21,12 @@ def clean_folder(file_formats: dict, curr_directory: str):
             if not file_extension.lower() in file_formats:
                 continue
 
-            target_path = f'{curr_directory}/{file_formats[file_extension.lower()]}'
+            target_path = f'{target_directory}/{file_formats[file_extension.lower()]}'
 
             if not os.path.isdir(target_path):
                 os.mkdir(target_path)
 
-            src_path = f'{curr_directory}/{d_file}'
+            src_path = f'{target_directory}/{d_file}'
             dst_path = f'{target_path}/{d_file}'
 
             shutil.copyfile(src=src_path, dst=dst_path)
@@ -36,14 +36,14 @@ def clean_folder(file_formats: dict, curr_directory: str):
         except:
             logger.log(f"Failed to move file {d_file}")
 
-    logger.log(f"Finished cleaned '{curr_directory}' folder")
+    logger.log(f"Finished cleaned '{target_directory}' folder")
 
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) < 2:
         raise Exception("Target folder must be passed to the command")
-    curr_directory = args[1]
+    target_directory = args[1]
     file_formats = dict()
 
     with open("file_formats.json", "r") as file:
@@ -52,4 +52,5 @@ if __name__ == '__main__':
     if len(file_formats) == 0:
         logger.log("Failed to execute because of missing file formats")
     else:
-        clean_folder(file_formats=file_formats, curr_directory=curr_directory)
+        clean_folder(file_formats=file_formats,
+                     target_directory=target_directory)
